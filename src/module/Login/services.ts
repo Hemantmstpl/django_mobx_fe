605bfdbed662e8d1f0504fa8ec166ef2ca8d3519
Hemant Motwani <hemantmstpl@gmail.com>
2020-05-28 14:56:52 +0530

    Remove usage of any from login service

--- src/module/Login/services.ts
+++ src/module/Login/services.ts
@@ -3,17 +3,25 @@ import Api from '../../utils/api';
 // import { AuthApi } from '@services/api/endpoints/auth-api-class';
 // import AuthPostParams = AuthApi.AuthPostParams;
 // import AuthTokenResponse = AuthApi.AuthTokenResponse;
+export interface loginSubData {
+    data: loginDataInterface
+}
+export interface loginDataInterface {
+    refresh: string;
+    access: string;
+}
+
 
 export class AuthLoginServices {
 
-    public static getToken = async (params: { username: string; password: string; }): Promise<any> => {
+    public static getToken = async (params: { username: string; password: string; }): Promise<loginSubData> => {
         return Api.post('/token/',{
             username: params.username,
             password: params.password,
         });
     };
 
-    public static refreshToken = async (): Promise<any> => {
+    public static refreshToken = async (): Promise<loginSubData> => {
         return Api.post('/token/refresh/', {"refresh": localStorage.getItem('refresh')});
       }
 
